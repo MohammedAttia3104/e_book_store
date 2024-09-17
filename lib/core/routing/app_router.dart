08037ced011d848883a0f5cbe65ec007de0ed7f8
MohamedAttia3104 <attia3104@gmail.com>
2024-09-17 05:21:40 +0300

    add app router

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,20 @@
+import 'package:e_book_store/core/routing/routes.dart';
+import 'package:flutter/material.dart';
+
+import '../../features/home/home_screen.dart';
+
+class AppRouter {
+  Route? generateRoute(RouteSettings settings) {
+    //this arguments to be passed in any screen like this ( arguments as ClassName )
+    final arguments = settings.arguments;
+
+    switch (settings.name) {
+      case Routes.homeScreen:
+        return MaterialPageRoute(
+          builder: (_) => const HomeScreen(),
+        );
+      default:
+        return null;
+    }
+  }
+}
