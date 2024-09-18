371241f47b7c3b6629c7f096746cab07a9e9abc2
MohamedAttia3104 <attia3104@gmail.com>
2024-09-18 11:22:28 +0300

    - add app colors

--- lib/core/theming/app_colors.dart
+++ lib/core/theming/app_colors.dart
@@ -2,4 +2,9 @@ import 'package:flutter/material.dart';
 
 abstract class AppColors {
   static const Color mainColor = Color(0xffEF5A5A);
+  static const Color primaryLightColor = Color(0xff19191B);
+  static const Color fontSecondaryLightColor = Color(0xff9D9D9D);
+  static const Color primaryDarkColor = Color(0xffFFFFFF);
+  // 15% opacity
+  static const Color txtFormFieldFillColor = Color(0xffC4C4C4);
 }
