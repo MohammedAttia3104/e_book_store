0d5d7390395d4085b6b8e37dc5c1cfc281ccc69c
MohamedAttia3104 <attia3104@gmail.com>
2024-09-26 09:41:42 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,15 @@ import 'package:bloc/bloc.dart';
 import 'package:e_book_store/core/di/service_locator.dart';
 import 'package:e_book_store/e_book_store.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'core/routing/app_router.dart';
 import 'core/utils/my_bloc_observer.dart';
 
-void main(){
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   setUpServiceLocator();
   Bloc.observer = MyBlocObserver();
-
+  await ScreenUtil.ensureScreenSize();
   runApp(EBookStore(appRouter: AppRouter()));
 }
